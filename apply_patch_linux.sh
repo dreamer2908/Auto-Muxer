dreamer2908/Auto-Muxer

commit 8b930f4b0631a0ba07b7fe8c31a61a49c1c529d9

sh support in linux and mac scripts is back. No longer check for output file before patching; overwriting existing file by default.

apply_patch_linux.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -o nounset
 # Do not use errexit here
 
@@ -6,7 +6,7 @@ set -o nounset
 # Enjoy your usual ratio: 5% of lines do the actual work, and the rest are there to make sure they work. (It's like 1%, actually)
 
 WORKINGDIR=$(pwd)
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPTDIR="$(cd "$(dirname "$0")" && pwd)"
 cd "$SCRIPTDIR"
 args="$@"
 
@@ -36,17 +36,17 @@ find_xdelta3() {
 }
 
 find_inputs() {
-	found=false
+	found=0
 	if [ ! -z "$args" ] && [ ! "$args" = " " ]; then
 		if [ -f "$args" ]; then
 			sourcefile=$@
-			found=true
+			found=1
 		else
 			echo "Warning: Input file \"$args\" is not found. Ignored."
-			found=false
+			found=0
 		fi
 	fi
-	if [ ! -f "$sourcefile" ] && [ $found == false ]; then
+	if [ ! -f "$sourcefile" ] && [ $found = 1 ]; then
 		if [ -f "../$sourcefile" ]; then
 			sourcefile="../$sourcefile"
 			targetfile="../$targetfile"
@@ -79,21 +79,6 @@ find_inputs() {
 	return 0
 }
 
-check_target_file() {
-	if [ -f "$targetfile" ]; then
-		echo "Target file \"$targetfile\" already exists."
-		if read -t 5 -p "Continue and overwrite? [y/n]" yn; then
-			if [[ $yn != y* ]]; then
-				echo "Aborted by user."
-				return 1
-			fi
-		else
-			echo "  User didn't answer. Continuing..."
-		fi
-	fi
-	return 0
-}
-
 run_patch () {
 	echo "Attempting to patch \"$sourcefile\"..."
 	`$app -d -f -s "$sourcefile" "$changes" "$targetfile"`
@@ -113,7 +98,7 @@ move_old_file () {
 	return 0
 }
 
-if find_xdelta3 && find_inputs && check_target_file; then
+if find_xdelta3 && find_inputs; then
 	if run_patch; then
 		if ! move_old_file; then
 			ignore=1

apply_patch_mac.command
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -o nounset
 # Do not use errexit here
 
@@ -6,7 +6,7 @@ set -o nounset
 # Enjoy your usual ratio: 5% of lines do the actual work, and the rest are there to make sure they work. (It's like 1%, actually)
 
 WORKINGDIR=$(pwd)
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPTDIR="$(cd "$(dirname "$0")" && pwd)"
 cd "$SCRIPTDIR"
 args="$@"
 
@@ -33,17 +33,17 @@ find_xdelta3() {
 }
 
 find_inputs() {
-	found=false
+	found=0
 	if [ ! -z "$args" ] && [ ! "$args" = " " ]; then
 		if [ -f "$args" ]; then
 			sourcefile=$@
-			found=true
+			found=1
 		else
 			echo "Warning: Input file \"$args\" is not found. Ignored."
-			found=false
+			found=0
 		fi
 	fi
-	if [ ! -f "$sourcefile" ] && [ $found == false ]; then
+	if [ ! -f "$sourcefile" ] && [ $found = 1 ]; then
 		if [ -f "../$sourcefile" ]; then
 			sourcefile="../$sourcefile"
 			targetfile="../$targetfile"
@@ -76,21 +76,6 @@ find_inputs() {
 	return 0
 }
 
-check_target_file() {
-	if [ -f "$targetfile" ]; then
-		echo "Target file \"$targetfile\" already exists."
-		if read -t 5 -p "Continue and overwrite? [y/n]" yn; then
-			if [[ $yn != y* ]]; then
-				echo "Aborted by user."
-				return 1
-			fi
-		else
-			echo "  User didn't answer. Continuing..."
-		fi
-	fi
-	return 0
-}
-
 run_patch () {
 	echo "Attempting to patch \"$sourcefile\"..."
 	`$app -d -f -s "$sourcefile" "$changes" "$targetfile"`
@@ -110,7 +95,7 @@ move_old_file () {
 	return 0
 }
 
-if find_xdelta3 && find_inputs && check_target_file; then
+if find_xdelta3 && find_inputs; then
 	if run_patch; then
 		if ! move_old_file; then
 			ignore=1

apply_patch_linux_alternative.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -o nounset
 set -o errexit
 
@@ -9,7 +9,7 @@ set -o errexit
 # (might not be correct, like tempSource.file, ???, or in the encoding it doesn't understand)
 
 WORKINGDIR=$(pwd)
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPTDIR="$(cd "$(dirname "$0")" && pwd)"
 cd "$SCRIPTDIR"
 args="$@"
 
@@ -39,14 +39,14 @@ find_xdelta3() {
 }
 
 find_inputs() {
-	found=false
+	found=0
 	if [ ! -z "$args" ] && [ ! "$args" = " " ]; then
 		if [ -f "$args" ]; then
 			sourcefile=$@
-			found=true
+			found=1
 		else
 			echo "Warning: Input file \"$args\" is not found. Ignored."
-			found=false
+			found=0
 		fi
 	fi
 	if [ ! -f "$changes" ]; then

apply_patch_mac_alternative.command
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -o nounset
 set -o errexit
 
@@ -9,7 +9,7 @@ set -o errexit
 # (might not be correct, like tempSource.file, ???, or in the encoding it doesn't understand)
 
 WORKINGDIR=$(pwd)
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPTDIR="$(cd "$(dirname "$0")" && pwd)"
 cd "$SCRIPTDIR"
 args="$@"
 
@@ -36,14 +36,14 @@ find_xdelta3() {
 }
 
 find_inputs() {
-	found=false
+	found=0
 	if [ ! -z "$args" ] && [ ! "$args" = " " ]; then
 		if [ -f "$args" ]; then
 			sourcefile=$@
-			found=true
+			found=1
 		else
 			echo "Warning: Input file \"$args\" is not found. Ignored."
-			found=false
+			found=0
 		fi
 	fi
 	if [ ! -f "$changes" ]; then

commit 20d6adbe1081174a02bc32784178865040ac7072

Fix the fuckup in commit 8b930f4 which broke linux and mac main scripts

apply_patch_linux.sh
@@ -36,17 +36,14 @@ find_xdelta3() {
 }
 
 find_inputs() {
-	found=0
 	if [ ! -z "$args" ] && [ ! "$args" = " " ]; then
 		if [ -f "$args" ]; then
 			sourcefile=$@
-			found=1
 		else
 			echo "Warning: Input file \"$args\" is not found. Ignored."
-			found=0
 		fi
 	fi
-	if [ ! -f "$sourcefile" ] && [ $found = 1 ]; then
+	if [ ! -f "$sourcefile" ]; then
 		if [ -f "../$sourcefile" ]; then
 			sourcefile="../$sourcefile"
 			targetfile="../$targetfile"

apply_patch_mac.command
@@ -33,17 +33,14 @@ find_xdelta3() {
 }
 
 find_inputs() {
-	found=0
 	if [ ! -z "$args" ] && [ ! "$args" = " " ]; then
 		if [ -f "$args" ]; then
 			sourcefile=$@
-			found=1
 		else
 			echo "Warning: Input file \"$args\" is not found. Ignored."
-			found=0
 		fi
 	fi
-	if [ ! -f "$sourcefile" ] && [ $found = 1 ]; then
+	if [ ! -f "$sourcefile" ]; then
 		if [ -f "../$sourcefile" ]; then
 			sourcefile="../$sourcefile"
 			targetfile="../$targetfile"